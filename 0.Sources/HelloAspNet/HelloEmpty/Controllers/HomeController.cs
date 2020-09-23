435b2c8015bf05d583005232656c296a206847e0
Pyunghwa Kim <syneffort@gmail.com>
2020-09-24 07:50:52 +0900

    MVC, Razor

---
+++ 0.Sources/HelloAspNet/HelloEmpty/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloEmpty.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HelloMessage msg = new HelloMessage()
+            {
+                Message = "Welcome to ASP.NET MVC!"
+            };
+
+            ViewBag.Noti = "Input message and click submit";
+
+            return View(msg);
+        }
+
+        // Post를 처리하는 Index
+        [HttpPost]
+        public IActionResult Index(HelloMessage obj)
+        {
+            ViewBag.Noti = "Message Changed";
+            return View(obj);
+        }
+    }
+}
