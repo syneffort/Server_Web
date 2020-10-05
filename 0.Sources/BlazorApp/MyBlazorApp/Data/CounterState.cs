d024df093b76fc742e23d56ebf406697faa3c994
Pyunghwa Kim <syneffort@gmail.com>
2020-10-06 08:32:35 +0900

    State 관리

---
+++ 0.Sources/BlazorApp/MyBlazorApp/Data/CounterState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlazorApp.Data
+{
+    public class CounterState
+    {
+        private int _count = 0;
+
+        public Action OnStateChanged;
+
+        private void Refresh()
+        {
+            OnStateChanged?.Invoke();
+        }
+
+        public int Count
+        {
+            get { return _count; }
+            set
+            {
+                _count = value;
+                Refresh();
+            }
+        }
+    }
+}
