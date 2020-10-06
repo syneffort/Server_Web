34b72dc70bf7bf9e4bd0f9b34795d706b3f9ee0f
Pyunghwa Kim <syneffort@gmail.com>
2020-10-07 08:44:27 +0900

    JS 연동

---
+++ 0.Sources/BlazorApp/MyBlazorApp/wwwroot/Test.js
@@ -0,0 +1,14 @@
+﻿
+var func = (function () {
+    window.testFunction = {
+        helloWorld: function () {
+            return alert('Hello World');
+        },
+        inputName: function (text) {
+            return prompt(text, 'input Name');
+        }
+    };
+
+});
+
+func();
